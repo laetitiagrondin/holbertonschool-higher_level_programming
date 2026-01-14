4c9d3ee907d707dff5006455ef71c719b455bd7c
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-01-14 14:32:41 +0000

    "else if" to " elif" added ":"

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10, 10)
 last_digit = abs(number) % 10
 if last_digit > 5:
     print("Last digit of", number, "is", last_digit, "and is greater than 5")
-else if last_digit == 0
+elif last_digit == 0:
     print("Last digit of", number, "is", last_digit, "and is 0")
 else:
     print("Last digit of", number, "is", last_digit, "and is less than 6 and not 0")
