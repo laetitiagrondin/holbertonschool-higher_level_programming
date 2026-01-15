1fd15410c66c45868ecc095ebec2761398066401
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-01-15 13:24:54 +0000

    1st commit

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argc = len(sys.argv) - 1
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+        print(f"1: {sys.argv[1]}")
+    else:
+        print(f"{argc} arguments:")
+        for i in range(1, argc + 1):
+            print(f"{i}: {sys.argv[i]}")
