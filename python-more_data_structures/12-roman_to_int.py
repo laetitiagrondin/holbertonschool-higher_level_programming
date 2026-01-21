353a92119a17490a9cba4ee307fe233be4f8a94f
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-01-21 12:39:31 +0000

    1st commit

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+
+    roman_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                 'C': 100, 'D': 500, 'M': 1000}
+
+    total = 0
+    i = 0
+    while i < len(roman_string):
+        value = roman_map.get(roman_string[i], 0)
+        if i + 1 < len(roman_string) and value < roman_map.get(roman_string[i + 1], 0):
+            total += roman_map[roman_string[i + 1]] - value
+            i += 2
+        else:
+            total += value
+            i += 1
+    return total
