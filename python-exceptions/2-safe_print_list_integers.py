10360f492d732a6db0cdf05df1ff78561b17116d
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-01-22 11:54:47 +0000

    ...

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,11 +3,10 @@ def safe_print_list_integers(my_list=[], x=0):
     total = 0
     for i in range(x):
         try:
-            try:
-                print("{:d}".format(my_list[i]), end="")
-                total += 1
-            except (ValueError, TypeError):
-                pass
+            print("{:d}".format(my_list[i]), end="")
+            total += 1
+        except (ValueError, TypeError):
+            pass
         except IndexError:
             break
     print()
