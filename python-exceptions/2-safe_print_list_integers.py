8d4b1c2661ff8fd8d1c14ab2eacc48813243e9bb
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-01-22 11:30:29 +0000

    Pass instead of continue, deleted ()

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,8 +6,8 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end="")
             total += 1
         except (ValueError, TypeError):
-            continue
-        except (IndexError):
+            pass
+        except IndexError:
             break
     print()
     return total
