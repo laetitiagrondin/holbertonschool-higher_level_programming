931ebc3ea546c3fa9475a8a8db7e73e705298603
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-01-23 19:53:40 +0000

    Indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -20,7 +20,7 @@ def text_indentation(text):
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    
+
     i = 0
     while i < len(text):
         print(text[i], end="")
