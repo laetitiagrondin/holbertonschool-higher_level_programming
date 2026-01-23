b5eeef68eed1ec619c9f6e775743ed6e3016e717
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-01-23 17:11:58 +0000

    Fi

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -25,12 +25,10 @@ def print_square(size):
     Returns:
     - This function does not return.
     """
-    if not isinstance(size, (int, float)):
-        raise ValueError("size must be >= 0")
-
-    if size < 0:
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
-
+    if size < 0:
+        raise ValueError("size must be >= 0")
     if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
     for i in range(size):
