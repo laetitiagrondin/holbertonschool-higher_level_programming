b91da42d9a565a1d53331ce770d24d95141ba4eb
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-01-23 20:08:10 +0000

    Fixed

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -20,12 +20,12 @@ def text_indentation(text):
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-
+    
     i = 0
     while i < len(text):
         print(text[i], end="")
         if text[i] in ".?:":
-            print("\n\n")
+            print("\n")
             i += 1
             while i < len(text) and text[i] == ' ':
                 i += 1
