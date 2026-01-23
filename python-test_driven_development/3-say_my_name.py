548344b79e9a373b75d1f0e9de779586dd31d4ab
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-01-23 13:56:23 +0000

    1st commit

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def say_my_name(first_name, last_name=""):
+    """
+    Function Description:
+    - Write a function that prints My name is <first name> <last name>
+    - Prototype: def say_my_name(first_name, last_name="").
+    - first_name and last_name must be strings, otherwise,
+        raise a TypeError exception with the message:
+        "first_name muste be a string" or "last_name must be a string".
+
+    Parameters:
+    - first_name: a string and the first name of the person.
+    - last_name: a string and the last name of the persone.
+
+    Returns:
+    - This function does not return.
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
