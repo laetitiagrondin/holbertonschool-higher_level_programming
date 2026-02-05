12d1a93459f11d7e6ab3a598ec3957dc099ddcd0
laetitiagrondin <laetitiagrondin93gmail.com>
2026-02-05 11:41:46 +0000

    Deleted ( )

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -18,7 +18,7 @@ class Circle(Shape):
         self.radius = radius
 
     def area(self):
-        return math.pi * (self.radius ** 2)
+        return math.pi * self.radius ** 2
 
     def perimeter(self):
         return 2 * math.pi * self.radius
