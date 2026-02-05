872273a8ea34a34281d3f3e02f302a528c0d3bbe
laetitiagrondin <laetitiagrondin93gmail.com>
2026-02-05 10:37:01 +0000

    1st commit and modified files

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+
+
+class VerboseList(list):
+    def append(self, item):
+        super().append(item)
+        print(f"Added [{item}] to the list.")
+
+    def extend(self, item):
+        x = len(item)
+        super().extend(item)
+        print(f"Extended the list with [{x}] items.")
+
+    def remove(self, item):
+        super().remove(item)
+        print(f"Removed [{item}] from the list.")
+
+    def pop(self, index = -1):
+        item = self[index]
+        print(f"Popped [{item}] from the list.")
+        return super().pop(index)
