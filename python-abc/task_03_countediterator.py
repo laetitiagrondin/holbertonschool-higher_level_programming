4fd1100aa6f9506babacf13646c3802188fe4b90
laetitiagrondin <laetitiagrondin93gmail.com>
2026-02-05 10:59:14 +0000

    Modified files + new file

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+
+
+class CountedIterator:
+    def __init__(self, iterator):
+        self.iterator = iter(iterator)
+        self.counted_iter = 0
+
+    def get_count(self):
+        return self.counted_iter
+
+    def __next__(self):
+        item = next(self.iterator)
+        self.counted_iter += 1
+        return item
