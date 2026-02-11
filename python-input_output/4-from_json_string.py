4f4040576700a737fe760a6a5f62e286e8db2cfc
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-02-11 09:28:09 +0000

    Documentation

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""
+Defines a function from_json_string that
+returns an object (Python data structure)
+represented by a JSON string.
+"""
 import json
 
 
 def from_json_string(my_str):
+    """
+    Function that returns an object (Python data structure)
+    represented by a JSON string.
+    """
     return json.loads(my_str)
