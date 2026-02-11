ec947c42dded1342d7881c61fff4d36f30ca99c6
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-02-11 10:12:34 +0000

    Documentation

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
+"""
+Defines a class Student that defines a student.
+"""
 
 
 class Student:
+    """
+    class Student that defines a student.
+    """
     def __init__(self, first_name, last_name, age):
+        """
+        Instantiation with public instance attributes.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """
+        Public method that retrieves a dictionary representation
+        of a Student instance.
+        """
         return self.__dict__
