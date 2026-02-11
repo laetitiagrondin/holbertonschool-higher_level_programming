df675b2012785c6352b21cc68377fe77405ba529
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-02-11 08:08:54 +0000

    Documentation

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""
+Defines a function append_write
+that appends a string at the end of a text file (UTF8)
+and returns the number of characters added.
+"""
+
 def append_write(filename="", text=""):
+    """
+    Function that appends a string at the end of a text file (UTF8)
+    and returns the number of characters added.
+    """
     with open(filename, "a", encoding="utf-8") as f:
         nb_characters_added = f.write(text)
     return nb_characters_added
