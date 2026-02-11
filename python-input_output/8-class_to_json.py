f895944c195e4522b20fab479b54043c791222b0
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-02-11 10:17:43 +0000

    Documentation

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
+"""
+Defines a function class_to_json that
+returns the dictionary description with simple data structure
+(list, dictionary, string, integer and boolean)
+for JSON serialization of an object.
+"""
+
+
 def class_to_json(obj):
+    """
+    Function that returns the dictionary description
+    with simple data structure (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object.
+    """
     return obj.__dict__
