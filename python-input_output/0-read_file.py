310b18a5bee5f58cb97b9d0f4d396f1e69c2ea12
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-02-11 08:01:40 +0000

    Documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+Defines a function read_file that
+reads a text file (UTF8) and prints it to stdout.
+"""
+
+
 def read_file(filename=""):
+    """
+    Function that reads a text file (UTF8) and prints it to stdout.
+    """
     with open(filename, "r", encoding="utf-8") as f:
         read_text = f.read()
     print(read_text, end="")
