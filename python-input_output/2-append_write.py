17b30e81e1ce12b0b7f67bb850f58e7d5671d01e
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-02-11 09:25:40 +0000

    Documentation + indented block

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -5,6 +5,7 @@ that appends a string at the end of a text file (UTF8)
 and returns the number of characters added.
 """
 
+
 def append_write(filename="", text=""):
     """
     Function that appends a string at the end of a text file (UTF8)
--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""
+Defines a function to_json_string that
+returns the JSON representation of an object (string).
+"""
 import json
 
 
 def to_json_string(my_obj):
+    """
+    Function that returns the JSON representation of an object (string).
+    """
     return json.dumps(my_obj)
