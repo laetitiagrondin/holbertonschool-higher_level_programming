ccf70b89e42d9ef042a189d90d9108a5356a9fc3
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-02-11 08:05:58 +0000

    Documentation

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""
+Defines a function write_file that writes a string to a text file (UTF8)
+and returns the number of characters written.
+"""
+
+
 def write_file(filename="", text=""):
+    """
+    Function that writes a string to a text file (UTF8)
+    and returns the number of characters written.
+    """
     with open(filename, "w", encoding="utf-8") as f:
         nb_characters = f.write(text)
     return nb_characters
