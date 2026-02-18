5dadee652b4036654f90fa4d8c5adcc1e2d2264b
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-02-18 14:33:08 +0000

    Deleted "."

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -33,7 +33,7 @@ class SimpleAPI(BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"Endpoint not found.".encode())
+            self.wfile.write(b"Endpoint not found".encode())
 
 
 if __name__ == "__main__":
