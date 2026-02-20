8492c9e9fd856d3247ca4a75f66a91495e0a1944
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-02-20 09:45:40 +0000

    Deleted status code from def login()

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -45,7 +45,7 @@ def login():
     if not user or not check_password_hash(user["password"], password):
         return jsonify({"error": "Invalid credentials"}), 401
     access_token = create_access_token(identity=username)
-    return jsonify(access_token=access_token), 200
+    return jsonify(access_token=access_token)
 
 
 @app.route("/jwt-required")
