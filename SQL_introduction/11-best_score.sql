874fc3e1ced2187cf39f984cb9d8c19c20fdf4bf
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-03-04 10:45:28 +0100

    Modified structure

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,2 +1,5 @@
 -- Script that lists all records with a score >= 10 in the table second_table.
-SELECT score, name FROM second_table ORDER BY score DESC WHERE score >= 10;
+SELECT score, name
+FROM second_table
+ORDER BY score DESC
+WHERE score >= 10;
