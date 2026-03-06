023f3122103e55ee129148db1cc8667f63cac3d1
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-03-06 20:49:01 +0100

    Added coma

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -7,7 +7,7 @@ from model_state import Base
 class City (Base):
     """Represents a city in the database."""
     __tablename__ = "cities"
-    id = Column(Integer, autoincrement=True, unique=True
+    id = Column(Integer, autoincrement=True, unique=True,
                 nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, nullable=False, ForeignKey("states.id"))
