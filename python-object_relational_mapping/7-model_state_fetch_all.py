5e42190c6d775517cbeaf0de34d41e41d58160ff
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-03-06 19:05:15 +0100

    Corrected errors for sys

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,7 +9,7 @@ from model_state import Base, State
 if __name__ == "__main__":
     engine = create_engine(
         "mysql+mysqldb://{}:{}@localhost/{}".format(
-            sys.argv[1], ss.argv[2], sys.argv[3]), pool_pre_ping=True)
+            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
     states = session.query(State).order_by(State.id).all()
