bdf8c0576ed9fefa07780c01486b99d5695da6dd
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-03-06 12:29:41 +0100

    Modification

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,29 +1,16 @@
 #!/usr/bin/python3
-"""Script that lists all states from the database hbtn_0e_0_usa"""
-
+"""Script that lists all states from the database hbtn_0e_0_usa."""
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    """Connect to the DB using command line arguments."""
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        pwd=sys.argv[2],
-        db=sys.argv[3]
-    )
-
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         pwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("""SELECT id, name
-                 FROM states
-                 ORDER BY states.id ASC""")
-
-    states = cur.fetchall()
-
-    for state in states:
-        print(state)
-
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
     cur.close()
     db.close()
