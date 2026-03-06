8cba5f3dd91c99d2dbe3320b569de4e7436e5205
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-03-06 18:17:59 +0100

    Fixed errors

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -4,10 +4,10 @@ import MySQLdb
 import sys
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
-    cur = conn.cursor()
+    cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name\
                 FROM cities\
                 LEFT JOIN states\
