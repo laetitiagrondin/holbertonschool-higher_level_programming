580cb1cc0003dc1c71bcc9a342cb3e4cf35d3aff
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-03-06 13:40:46 +0100

    Correction

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
 """Script that lists all states with a name
-starting with N (upper N) from the database hbtn_0e_0_usa"""
-
+starting with upper N from the database hbtn_0e_0_usa."""
 import MySQLdb
 import sys
 
-if __name__ = "__main__":
-"""Connect to the DB using command line arguments."""
-db = MySQdb.connect(
-host="localhost",
-port=3306
-user=sys.argv[1],
-pwd=sys.argv[2],
-db=sys.argv[3]
-)
 
-cur = db.cursor()
-cur.execute("SELECT * FROM states WHERE name = 'N%' ORDER BY states.id ASC;")
-
-states = cur.fetchall()
-
-for state in states:
-print(state)
-
-cur.close()
-db.close();
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd='', db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name\
+                 LIKE BINARY 'N%' ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db.close()
