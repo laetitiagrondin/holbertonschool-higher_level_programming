1cf5391276cdf4ca2d386efc01bbac8f0cc0e73c
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-03-06 18:27:33 +0100

    Fixed errors

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -5,6 +5,7 @@ using the database hbtn_0e_4_usa."""
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
@@ -12,7 +13,7 @@ if __name__ == "__main__":
     cur.execute("SELECT * FROM states WHERE states.name LIKE BINARY '{}'\
                 ORDER BY id ASC".format(sys.argv[4]))
     query_rows = cur.fetchall()
-    for row in rows:
+    for row in query_rows:
         print(row)
     cur.close()
     db.close()
