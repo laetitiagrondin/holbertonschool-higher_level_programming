2960005299e4b3ba1db2c596377eccbe519e5f62
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-03-06 19:44:04 +0100

    First commit

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Script that prints the State object with the name
+passed as argument from the database hbtn_0e_6_usa."""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]), sys.argv[4],
+            pool_pre_ping=True)
+    Session = sessionmaker(bine=engine)
+    session = Session()
+    state = session.query(State).filter(
+        State.name == state_name).first()
+    if not state:
+        print("Not found")
+    else:
+        print(state.id)
+    session.close()
