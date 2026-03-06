2b6dc9adb58ff386f9a1a51be49220d121373a93
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-03-06 18:34:29 +0100

    Fixed errors

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -10,8 +10,12 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE states.name LIKE BINARY '{}'\
-                ORDER BY id ASC".format(sys.argv[4]))
+    cur.execute("SELECT cities.name\
+                FROM cities\
+                LEFT JOIN states\
+                ON states.id = cities.state_id\
+                WHERE states.name =%s\
+                ORDER BY cities.id ASC", (sys.argv[4],))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
