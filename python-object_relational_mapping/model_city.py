0ef2dc779ca432bcd29158e767a0787f574d4468
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-03-06 20:45:04 +0100

    First commit and deletes 1 file

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Contains the class definition of a City."""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City (Base):
+    """Represents a city in the database."""
+    __tablename__ = "cities"
+    id = Column(Integer, autoincrement=True, unique=True
+                nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, nullable=False, ForeignKey("states.id"))
