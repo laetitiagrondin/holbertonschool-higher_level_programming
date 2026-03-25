8876d59620283ebc6315795374838670b06b324b
laetitiagrondin <laetitiagrondin93@gmail.com>
2026-03-25 15:04:40 +0100

    Commit

---
+++ python-server_side_rendering/task_00_intro.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def generate_invitations(template, attendees):
+    if not isinstance(template, str):
+        print("Invalid input type: template should be a string.")
+        return
+    if not isinstance(attendees, list):
+        print("Invalid input type: template should be a list of dictionaries.")
+        return
+    if not template:
+        print("Template is empty, no output fils generated.")
+        return
+    if not attendees:
+        print("No data provided, no output files generated.")
+        return
+    for i, attendee in enumerate(attendees, start=1):
+        content = template
+        for key in ["name", "event_title", "event_date", "event_location"]:
+            value = attendee.get(key)
+            if value is None:
+                value = "N/A"
+            content = content.replace(f"{{{key}}}", str(vaule))
+        with open(f"output_{i}.txt", "w") as file:
+            file.wrtie(content)
